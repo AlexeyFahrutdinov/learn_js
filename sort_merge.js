1b0a6be247efbe04c9cab2ca5375faab04a427cb
AlexeyFahrutdinov <fahrutdinov2007@ya.ru>
2023-11-11 20:39:46 +0700

    first commit

---
+++ sort_merge.js
@@ -0,0 +1,17 @@
+let arr1 = [1, 3 ,5, 7, 13];
+let arr2 = [2, 4, 6, 8];
+let i = 0; j =0;
+result = []
+while(i < arr1.length || j < arr2.length){
+    if(arr1[i] < arr2[j] || arr2[j] === undefined){
+        result.push(arr1[i]);
+        i++;
+        console.log(i)
+    }
+    else if(arr2[j] < arr1[i] || arr1[i] === undefined) {
+        result.push(arr2[j]);
+        j++;
+        console.log(j)
+    }
+}
+console.log(result);
